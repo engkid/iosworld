24e87473a126d8a2d42ccd77f1ff965e83a67dd6
Engkit Satia Riswara <engkidsr@gmail.com>
2026-03-03 12:45:40 +0700

    create Feed module

--- iOSWorld/Modules/Feed/Package.swift
+++ iOSWorld/Modules/Feed/Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Home",
+    name: "Feed",
     platforms: [
       .iOS(.v26)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Home",
-            targets: ["Home"]
+            name: "Feed",
+            targets: ["Feed"]
         ),
     ],
     dependencies: [
@@ -23,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Home",
+            name: "Feed",
             dependencies: [
               .product(name: "NetworkService", package: "NetworkService"),
               .product(name: "Core", package: "Core")
